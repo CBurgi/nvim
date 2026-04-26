607818652e6e8a1a5377e1c9753a8f322618d0de
Cole Burgi <cburg@Framework13-Debian.home>
2026-04-26 00:21:39 -0400

    moved minimap to seperate split to fix overlap issue

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -1,21 +1,25 @@
 return {
-	"Isrothy/neominimap.nvim",
-	init = function ()
-		vim.g.neominimap = {
-			auto_enable = true,
-      click = {
-        enabled = true,
-        auto_switch_focus = false,
-      },
-      mark = {
-        enabled = true,
-      },
-      search = {
-        enabled = true,
-      },
-		}
-		vim.keymap.set('n', '<leader>mm', "<cmd>Neominimap ToggleFocus<CR>", {})
-		vim.keymap.set('n', '<leader>mt', "<cmd>Neominimap Toggle<CR>", {})
-		vim.keymap.set('n', '<leader>mr', "<cmd>Neominimap Refresh<CR>", {})
-	end
+      "Isrothy/neominimap.nvim",
+      init = function()
+            vim.g.neominimap = {
+                  auto_enable = true,
+                  layout = "split",
+                  split = {
+                        close_if_last_window = true
+                  },
+                  click = {
+                        enabled = true,
+                        auto_switch_focus = false,
+                  },
+                  mark = {
+                        enabled = true,
+                  },
+                  search = {
+                        enabled = true,
+                  },
+            }
+            vim.keymap.set('n', '<leader>mm', "<cmd>Neominimap ToggleFocus<CR>", {})
+            vim.keymap.set('n', '<leader>mt', "<cmd>Neominimap Toggle<CR>", {})
+            vim.keymap.set('n', '<leader>mr', "<cmd>Neominimap Refresh<CR>", {})
+      end
 }
